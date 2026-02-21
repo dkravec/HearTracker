cb57f8ae3ff6faae16684a0864b314624f8f1e70
Daniel Kravec <daniel@kravec.com>
2026-02-21 22:20:23 +0000

    Initial Commit

---
+++ HearingAidBattery/HearingAidBatteryApp.swift
@@ -0,0 +1,32 @@
+//
+//  HearingAidBatteryApp.swift
+//  HearingAidBattery
+//
+//  Created by Daniel Kravec on 2026-02-21.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct HearingAidBatteryApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ HearingAidBattery/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  HearingAidBattery
+//
+//  Created by Daniel Kravec on 2026-02-21.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
